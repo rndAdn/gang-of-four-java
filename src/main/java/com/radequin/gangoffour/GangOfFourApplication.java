d42619a1bff573c9d7c543cd0549833caf86e972
radequin <radequin@excilys.com>
2018-09-22 15:07:10 +0200

    empty project

---
+++ src/main/java/com/radequin/gangoffour/GangOfFourApplication.java
@@ -0,0 +1,12 @@
+package com.radequin.gangoffour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangOfFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GangOfFourApplication.class, args);
+	}
+}
